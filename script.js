d052627e48788fe88bafc3a0016cce4b76d9ef03
vinhb <vinh.bui10@gmail.com>
2022-01-17 13:02:03 -0800

    define grid functionality

--- script.js
+++ script.js
@@ -7,25 +7,62 @@ const CONTAINER = document.createElement('div');
 CONTAINER.classList.add('container')
 const BODY = document.querySelector('body');
 const JS = document.querySelector('script');
+const BUTTON = document.createElement('button');
+BUTTON.innerText = 'Reset'
+let userRow;
+let userColumn;
 
+BODY.insertBefore(BUTTON, JS);
+BODY.insertBefore(CONTAINER, JS);
 
-// need to create 16 columns
-// within each column, 16 boxes
-
-function createGrid(row,col) {
-    for (let i = 0; i < row; i++) {
-        let row = document.createElement('div');
-        row.classList.add('pixel', 'row');
-        
-        for (let j = 0; j < col; j++) {
-            let column = document.createElement('div');
-            column.classList.add('pixel', 'column');
-            row.appendChild(column);
-        }
-        CONTAINER.appendChild(row);
+function reset() {
+    while (CONTAINER.firstChild) {
+        CONTAINER.removeChild(CONTAINER.firstChild);
     }
 }
-    
-createGrid(16,16);
 
-BODY.insertBefore(CONTAINER, JS);
+function draw() {
+    userRow = prompt('How many rows should be in the grid?')
+    while (userRow.includes('.') ||
+            !parseInt(userRow, 10) || 
+            userRow > 100) { 
+        userRow = prompt('Please enter a valid integer less than 100.');
+        
+    }
+    
+    userColumn = prompt('How many columns should be in the grid?') 
+    while (userColumn.includes('.') || 
+            !parseInt(userColumn, 10) || 
+            userRow > 100) {
+        userColumn = parseInt(prompt('Please enter a valid integer less than 100.'))
+    }
+}
+
+function pixelTransform(node) {
+    node.target.classList.add('hover'); // adds css class to color in pixel
+}
+
+function createGrid(row,col) {
+    for (let i = 0; i < col; i++) {
+        let col = document.createElement('div');
+        col.classList.add('pixel', 'column');
+        
+        for (let j = 0; j < row; j++) {
+            let row = document.createElement('div');
+            row.classList.add('pixel', 'row');
+            row.addEventListener('mouseover', pixelTransform)
+            col.appendChild(row);
+        }
+        CONTAINER.appendChild(col);
+    }
+}
+
+BUTTON.addEventListener('click', () => {
+    reset();
+    draw();
+    createGrid(userRow,userColumn);
+});
+
+
+
+
